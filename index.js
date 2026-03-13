e90cfb71ea4e0199f5cff7e7baee572a81021e55
misshappynewyear <misshappynewyear@gmail.com>
2026-03-13 11:40:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -40,8 +40,10 @@ const MAX_PROMPT_LENGTH = 300;
 const MAX_TURN_LENGTH = 1500;
 
 const VOTE_EMOJIS = [
-  '1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣',
-  '6️⃣', '7️⃣', '8️⃣', '9️⃣',
+  '0️⃣',
+  '1️⃣','2️⃣','3️⃣','4️⃣','5️⃣',
+  '6️⃣','7️⃣','8️⃣','9️⃣',
+  '🔟'
 ];
 
 let lastDiscordReadyAt = null;
